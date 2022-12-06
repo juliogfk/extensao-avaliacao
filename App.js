ab11a2b7dfd466981728044e563238d5fc483e22
juliogfk <juliogfk1@gmail.com>
2022-12-06 17:55:19 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import reactDom from 'react-dom';
+import { StyleSheet, Text, View} from 'react-native';
+import {Button, Input, Avatar} from 'react-native-elements'
 
-export default function App() {
+const Flex = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  <View style={[styles.container, {
+    flexDirection: "column"
+    }]}>
+    <View style={{ flex: 2, display:"flex", justifyContent:"center", alignItems: "center",}} >
+      <Avatar
+        avatarStyle={styles.avatar}
+        size={300}
+        rounded
+        icon={{name: 'user', type: 'font-awesome',color:'black'}}
+        onPress={() => console.log("Works!")}
+        activeOpacity={0.7}
+        containerStyle={{flex: 2, marginLeft: 20, marginTop: 115}}
+      />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    </View>
+    
+    <View style={{ flex: 1, padding: 25, paddingBottom: 30 }}>
+      <Text style={{display:"flex",alignItems:"center",fontSize: 20, padding:10}}>
+          Login
+      </Text>
+      <Input style={{backgroundColor:"white",border: "solid"}}></Input>
+      <Text style={{display:"flex",alignItems:"center",fontSize:20, padding:10}}>
+          Senha
+      </Text>
+      <Input style={{backgroundColor:"white",border: "solid"}}></Input>
+    </View>
+    
+    <View style={{ flex: 1, padding: 30,}} >
+      <Button title="Login" style={{paddingBottom: 20,}}  />
+      <Button title="Cadastre-se" />
+    </View>
+  </View>
+  );
+  };
+  
+  const styles = StyleSheet.create({
+  container: {  
+  flex: 1,
+  justifyContent: "center",
+  padding: 20,
+  backgroundColor: "#f2dcd5",
   },
-});
+  
+
+  });
+
+  
+  export default Flex;
